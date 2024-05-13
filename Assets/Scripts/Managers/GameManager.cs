11dc51723cc2d28e39e5513abb5948c09389fccf
twotop6357 <twotop6357@gmail.com>
2024-05-13 11:09:05 +0900

    Update MakeEnemy
    적이 랜덤 위치에 생성되도록 구현

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
+    public GameObject skeleton;
+    public GameObject archer;
     [SerializeField] private string playerTag;
     public ObjectPool ObjectPool {  get; private set; }
     public Transform Player { get; private set; }
@@ -17,5 +19,27 @@ public class GameManager : MonoBehaviour
         }
         Player = GameObject.FindGameObjectWithTag(playerTag).transform;
         ObjectPool = GetComponent<ObjectPool>();
+    }
+    private void Start()
+    {
+        InvokeRepeating("MakeEnemy", 1f, 1.0f);
+    }
+    private void MakeEnemy()
+    {
+        int rand = Random.Range(0, 10);
+        float randPosX = Random.Range(3f, 6.5f);
+        float randPosY = Random.Range(-2f, 2f);
+        Vector2 pos = new Vector2(randPosX, randPosY);
+        
+        if (rand < 3)
+        {
+            archer.transform.position = pos;
+            Instantiate(archer, transform);
+        }
+        else
+        {
+            skeleton.transform.position = pos;
+            Instantiate(skeleton, transform);
+        }
     }
 }
